81b01c90da69665d409bf43c6554e8caa7465886
ablaszkiewicz <kqmdjc8@gmail.com>
2025-09-19 16:44:22 +0200

    feat: refactor exception_capture file

---
+++ main2.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/env ruby
+
+require 'bundler/setup'
+require 'posthog'
+require 'logger'
+
+posthog = PostHog::Client.new({
+  api_key: 'phc_VXlGk6yOu3agIn0h7lTmSOECAGWCtJonUJDAN4CexlJ',
+  host: 'http://localhost:8010',
+})
+
+posthog.logger.level = Logger::DEBUG
+
+
+begin
+  raise StandardError, "This is a test2 exception!"
+rescue => e
+  posthog.capture_exception(e, 'test-user-123')
+end
+
+posthog.flush
--- posthog-ruby-main/lib/posthog/exception_capture.rb
+++ posthog-ruby-main/lib/posthog/exception_capture.rb
@@ -4,14 +4,10 @@ require 'json'
 
 module PostHog
   module ExceptionCapture
-    # Builds exception properties in a format compatible with PostHog's exception tracking
-    # Based on the Python SDK implementation for consistency
     def self.build_exception_properties(exception, additional_properties = {})
-      # Get the exception details
       exception_list = build_exception_list(exception)
       
-      # Build the main properties following Python SDK format
-      # Extract types and messages from exception list for backwards compatibility
+      # todo: should it be array?
       exception_types = exception_list.map { |exc| exc['type'] }
       exception_values = exception_list.map { |exc| exc['value'] }
       
@@ -24,7 +20,6 @@ module PostHog
       $stderr.puts "\n[PostHog::ExceptionCapture] Exception properties:"
       $stderr.puts JSON.pretty_generate(properties)
 
-      # Merge any additional properties
       properties.merge!(additional_properties) if additional_properties
 
       properties
@@ -33,37 +28,25 @@ module PostHog
     private
 
     def self.build_exception_list(exception)
-      # Build a list of exceptions (in Ruby we typically have just one)
-      # but we format it as a list to match the Python SDK
       exceptions = []
       
-      # Process the main exception
       exceptions << build_single_exception(exception)
       
-      # If there's a cause (Ruby 2.1+), add it to the chain
-      if exception.respond_to?(:cause) && exception.cause
-        current = exception.cause
-        while current
-          exceptions << build_single_exception(current, true)
-          current = current.respond_to?(:cause) ? current.cause : nil
-        end
-      end
-      
-      # Reverse the list so the root cause is first (matching Python SDK behavior)
-      exceptions.reverse
+      # todo: if there's a cause (Ruby 2.1+), add it to the chain
+      exceptions
     end
 
-    def self.build_single_exception(exception, is_cause = false)
+    def self.build_single_exception(exception)
       exc_info = {
         'type' => exception.class.name,
         'value' => exception.message,
         'mechanism' => {
-          'type' => is_cause ? 'chained' : 'generic',
+          # todo: if there is cause, use 'chained'
+          'type' => 'generic',
           'handled' => true
         }
       }
       
-      # Add stack trace if available
       if exception.backtrace && !exception.backtrace.empty?
         exc_info['stacktrace'] = {
           'type' => 'raw',
@@ -75,12 +58,10 @@ module PostHog
     end
 
     def self.build_stack_frames(backtrace)
-      # Take first 100 frames to avoid massive payloads
       frames = backtrace.first(100).map do |line|
         parse_backtrace_line(line)
       end.compact
       
-      # Python SDK returns frames in reverse order (innermost last)
       frames.reverse
     end
 
@@ -112,10 +93,8 @@ module PostHog
         return nil
       end
       
-      # Add in_app flag
       frame['in_app'] = !is_library_frame?(frame['abs_path'] || frame['filename'])
       
-      # Try to add context lines if possible
       if frame['abs_path'] && File.exist?(frame['abs_path'])
         add_context_lines(frame, frame['abs_path'], frame['lineno'])
       end
@@ -124,14 +103,12 @@ module PostHog
     end
     
     def self.clean_function_name(function_name)
-      # Clean up function names like "block in method" or "block (2 levels) in method"
       function_name
         .gsub(/^block\s*(\(\d+ levels?\))?\s*in\s*/, '')
         .gsub(/[`']/, '')
     end
     
     def self.get_relative_path(path)
-      # Try to make path relative to current working directory
       if path.start_with?(Dir.pwd)
         path.sub(Dir.pwd + '/', '')
       else
@@ -140,7 +117,6 @@ module PostHog
     end
 
     def self.is_library_frame?(path)
-      # Mark frames as library frames if they're from gems or ruby internals
       path.include?('/gems/') ||
         path.include?('/rubies/') ||
         path.include?('/ruby/') ||
@@ -157,26 +133,22 @@ module PostHog
       begin
         lines = File.readlines(file_path)
         
-        # Get context lines (5 before and after, like Python SDK)
         start_line = [lineno - 5, 1].max
         end_line = [lineno + 5, lines.length].min
         
         if lineno > 0 && lineno <= lines.length
-          # Arrays are 0-indexed, line numbers are 1-indexed
           frame['context_line'] = lines[lineno - 1].chomp if lines[lineno - 1]
           
-          # Pre-context (lines before)
           if start_line < lineno
             frame['pre_context'] = lines[(start_line - 1)...(lineno - 1)].map(&:chomp)
           end
           
-          # Post-context (lines after)
           if end_line > lineno
             frame['post_context'] = lines[lineno...(end_line)].map(&:chomp)
           end
         end
       rescue => e
-        # Silently ignore if we can't read the file
+        # todo: we can't read the file. Do something with it.
       end
     end
   end
